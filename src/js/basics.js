a3663d346335e7598d62a6ba1372ff4af974efb2
Rhondel Divinas Flores <sc.rhondel.divinasflores@cvsu.edu.ph>
2026-02-18 12:21:00 +0800

    Refactor API versioning and improve code consistency in basics.js and departments.js

--- src/js/basics.js
+++ src/js/basics.js
@@ -1,15 +1,11 @@
-const API_VERSION = 'v1'
+const API_VERSION = "v1";
 
 // this is for development only : start.
-const DEV = true
+const DEV = true;
 
 // const base_url = 'http://localhost:3000'
-const base_url = 'http://192.168.1.20:3000' // my binded local server's network ip
+const base_url = "http://192.168.1.14:3000"; // my binded local server's network ip
 
 // this is for development only : end.
 
-export {
-  API_VERSION,
-  DEV,
-  base_url,
-}
+export { API_VERSION, DEV, base_url };
--- src/js/departments.js
+++ src/js/departments.js
@@ -1,76 +1,76 @@
 import { DEV, API_VERSION, base_url } from "./basics.js";
 
 export async function loginDepartment(payload) {
-  console.log('login department ', payload)
+  console.log("login department ", payload);
 
-  let api_request = `/auth_gasss_login`
+  let api_request = `/auth_gasss_login`;
 
   if (DEV) {
-    console.log('call: loginDepartment')
-    api_request = `${base_url}/auth_gasss_login`
+    console.log("call: loginDepartment");
+    api_request = `${base_url}/auth_gasss_login`;
   }
 
   const response = await fetch(api_request, {
-    method: 'POST',
+    method: "POST",
     credentials: "include",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(payload)
+    body: JSON.stringify(payload),
   });
 
-  return response
+  return response;
 }
 
 export async function logoutDepartment() {
-  console.log('logout department ')
+  console.log("logout department ");
 
-  let api_request = `/auth_gasss_logout`
+  let api_request = `/auth_gasss_logout`;
 
   if (DEV) {
-    console.log('call: logoutDepartment')
-    api_request = `${base_url}/auth_gasss_logout`
+    console.log("call: logoutDepartment");
+    api_request = `${base_url}/auth_gasss_logout`;
   }
 
   const response = await fetch(api_request, {
-    method: 'DELETE',
-    credentials: "include"
+    method: "DELETE",
+    credentials: "include",
   });
 
-  return response
+  return response;
 }
 
 export async function fetchWho() {
-  console.log('who? ')
+  console.log("who? ");
 
-  let api_request = `/who`
+  let api_request = `/who`;
 
   if (DEV) {
-    console.log('call: fetchWho')
-    api_request = `${base_url}/who`
+    console.log("call: fetchWho");
+    api_request = `${base_url}/who`;
   }
 
   const response = await fetch(api_request, {
-    method: 'GET',
+    method: "GET",
     credentials: "include",
     headers: {
       Accept: "text/plain",
-    }
+    },
   });
 
   if (!response.ok) {
     throw Error(`${response.status} :${await response.text()}`);
   }
 
-  return response.text()
+  return response.text();
 }
 
 export async function fetchAllDepartments() {
-  let api_request = `/${API_VERSION}/all_departments`
+  let api_request = `/${API_VERSION}/all_departments`;
 
   if (DEV) {
-    console.log('call: fetchAllDepartments')
-    api_request = `${base_url}/${API_VERSION}/all_departments`
+    console.log("call: fetchAllDepartments");
+    api_request = `${base_url}/${API_VERSION}/all_departments`;
   }
 
   const response = await fetch(api_request, {
@@ -78,10 +78,11 @@ export async function fetchAllDepartments() {
       Accept: "application/json",
     },
     credentials: "include",
-    method: 'GET'
+    method: "GET",
   });
 
   if (!response.ok) {
+    console.log(await response.text());
     throw Error(`${response.status} :${await response.text()}`);
   }
 
@@ -89,11 +90,11 @@ export async function fetchAllDepartments() {
 }
 
 export async function fetchDepartmentCurriculumsData(department_id, semester) {
-  let api_request = `/${API_VERSION}/department_data?department_id=${department_id}&semester=${semester}`
+  let api_request = `/${API_VERSION}/department_data?department_id=${department_id}&semester=${semester}`;
 
   if (DEV) {
-    console.log('call: fetchDepartmentData')
-    api_request = `${base_url}/${API_VERSION}/department_data?department_id=${department_id}&semester=${semester}`
+    console.log("call: fetchDepartmentData");
+    api_request = `${base_url}/${API_VERSION}/department_data?department_id=${department_id}&semester=${semester}`;
   }
 
   const response = await fetch(api_request, {
@@ -101,7 +102,7 @@ export async function fetchDepartmentCurriculumsData(department_id, semester) {
       Accept: "application/json",
     },
     credentials: "include",
-    method: 'GET'
+    method: "GET",
   });
 
   if (!response.ok) {
@@ -111,23 +112,30 @@ export async function fetchDepartmentCurriculumsData(department_id, semester) {
   return await response.json();
 }
 
-export async function fetchDepartmentsPaginated(page_size, page, code_match = "", name_match = "") {
-  let api_request = `/${API_VERSION}/departments?page_size=${page_size}&page=${page}`
+export async function fetchDepartmentsPaginated(
+  page_size,
+  page,
+  code_match = "",
+  name_match = "",
+) {
+  let api_request = `/${API_VERSION}/departments?page_size=${page_size}&page=${page}`;
 
   if (DEV) {
-    console.log('call: fetchSubjects')
-    api_request = `${base_url}/${API_VERSION}/departments?page_size=${page_size}&page=${page}`
+    console.log("call: fetchSubjects");
+    api_request = `${base_url}/${API_VERSION}/departments?page_size=${page_size}&page=${page}`;
   }
 
-  if (code_match) api_request += `&code_match=${encodeURIComponent(code_match)}`;
-  if (name_match) api_request += `&name_match=${encodeURIComponent(name_match)}`;
+  if (code_match)
+    api_request += `&code_match=${encodeURIComponent(code_match)}`;
+  if (name_match)
+    api_request += `&name_match=${encodeURIComponent(name_match)}`;
 
   const response = await fetch(api_request, {
     headers: {
       Accept: "application/json",
     },
     credentials: "include",
-    method: 'GET'
+    method: "GET",
   });
 
   if (!response.ok) {
@@ -138,16 +146,16 @@ export async function fetchDepartmentsPaginated(page_size, page, code_match = ""
 }
 
 export async function deleteRemoveDepartment(department_id) {
-  let api_request = `/${API_VERSION}/department_remove?department_id=${department_id}`
+  let api_request = `/${API_VERSION}/department_remove?department_id=${department_id}`;
 
   if (DEV) {
-    console.log('call: deleteRemoveDepartment')
-    api_request = `${base_url}/${API_VERSION}/department_remove?department_id=${department_id}`
+    console.log("call: deleteRemoveDepartment");
+    api_request = `${base_url}/${API_VERSION}/department_remove?department_id=${department_id}`;
   }
 
   const response = await fetch(api_request, {
     credentials: "include",
-    method: 'DELETE',
+    method: "DELETE",
     headers: {
       Accept: "text/plain",
     },
@@ -159,22 +167,22 @@ export async function deleteRemoveDepartment(department_id) {
 }
 
 export async function postCreateDepartment(department) {
-  let api_request = `/${API_VERSION}/department_add`
+  let api_request = `/${API_VERSION}/department_add`;
 
   if (DEV) {
-    console.log('call: postCreateDepartment')
-    api_request = `${base_url}/${API_VERSION}/department_add`
+    console.log("call: postCreateDepartment");
+    api_request = `${base_url}/${API_VERSION}/department_add`;
   }
 
   const response = await fetch(api_request, {
     credentials: "include",
-    method: 'POST',
+    method: "POST",
     headers: {
       Accept: "text/plain",
       "Content-Type": "application/json",
     },
 
-    body: JSON.stringify(department)
+    body: JSON.stringify(department),
   });
 
   if (!response.ok) {
@@ -183,22 +191,22 @@ export async function postCreateDepartment(department) {
 }
 
 export async function patchUpdateDepartment(department) {
-  let api_request = `/${API_VERSION}/department_update`
+  let api_request = `/${API_VERSION}/department_update`;
 
   if (DEV) {
-    console.log('call: patchUpdateDepartment')
-    api_request = `${base_url}/${API_VERSION}/department_update`
+    console.log("call: patchUpdateDepartment");
+    api_request = `${base_url}/${API_VERSION}/department_update`;
   }
 
   const response = await fetch(api_request, {
     credentials: "include",
-    method: 'PATCH',
+    method: "PATCH",
     headers: {
       Accept: "text/plain",
       "Content-Type": "application/json",
     },
 
-    body: JSON.stringify(department)
+    body: JSON.stringify(department),
   });
 
   if (!response.ok) {
