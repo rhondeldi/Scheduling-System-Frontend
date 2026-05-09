b82a307957a2ced76f06ded47c404617cdb8102a
Rhondel Divinas Flores <sc.rhondel.divinasflores@cvsu.edu.ph>
2026-05-09 18:18:47 +0800

    refactor: improve code readability and formatting in CurriculumsTableList and Departments components

--- src/departments/Departments.jsx
+++ src/departments/Departments.jsx
@@ -45,7 +45,11 @@ import {
 import { MainHeader } from "../components/Header";
 
 const truncateText = (text, maxLength) =>
-  text ? (text.length > maxLength ? text.substring(0, maxLength) + "..." : text) : "";
+  text
+    ? text.length > maxLength
+      ? text.substring(0, maxLength) + "..."
+      : text
+    : "";
 
 export default function Departments() {
   const [mode, setMode] = useState("new");
@@ -55,7 +59,7 @@ export default function Departments() {
   const [popupOptions, setPopupOptions] = useState(null);
   const [isOperationLoading, setIsOperationLoading] = useState(false);
   const [department, setDepartment] = useState({
-    DepartmentID: null,
+    DepartmentID: 0,
     Code: "",
     Name: "",
   });
@@ -75,7 +79,12 @@ export default function Departments() {
     setIsLoading(true);
 
     try {
-      const data = await fetchDepartmentsPaginated(size, currentPage, search, "");
+      const data = await fetchDepartmentsPaginated(
+        size,
+        currentPage,
+        search,
+        "",
+      );
 
       setDepartmentList(data.Departments || []);
       setTotalCount(data.TotalDepartments || 0);
@@ -153,210 +162,244 @@ export default function Departments() {
   return (
     <>
       <MainHeader pageName="departments">
+        <Popup
+          popupOptions={popupOptions}
+          closeButtonActionHandler={() => setPopupOptions(null)}
+        />
 
-      <Popup
-        popupOptions={popupOptions}
-        closeButtonActionHandler={() => setPopupOptions(null)}
-      />
-
-      <Box>
-        <Box sx={{ display: "flex", justifyContent: "space-between", p: 2 }}>
-          <TextField
-            size="small"
-            label="Search department"
-            value={searchTerm}
-            onChange={(e) => {
-              setPage(0);
-              setSearchTerm(e.target.value);
-            }}
-          />
-
-          <Button
-            variant="contained"
-            startIcon={<AddIcon />}
-            onClick={() => {
-              setMode("new");
-              setDepartment({ Code: "", Name: "" });
-              setIsDialogFormOpen(true);
-            }}
-          >
-            Add Department
-          </Button>
-        </Box>
-
-        <Box px={4}>
-          <TableContainer component={Paper}>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>CODE</TableCell>
-                  <TableCell>NAME</TableCell>
-                  <TableCell />
-                </TableRow>
-              </TableHead>
-
-              <TableBody>
-                {isLoading ? (
-                  <TableRow>
-                    <TableCell colSpan={3} align="center">
-                      <CircularProgress />
-                    </TableCell>
-                  </TableRow>
-                ) : departmentList.length === 0 ? (
-                        <TableRow>
-                            <TableCell colSpan={6} align="center" sx={{ fontWeight: 'bold', py: 15 }}>
-                                No departments found.
-                            </TableCell>
-                        </TableRow>
-                ) : (
-                  departmentList.map((d) => (
-                    <TableRow key={d.DepartmentID}>
-                      <TableCell sx={{ fontWeight: "bold" }}>{d.Code}</TableCell>
-                      <TableCell sx={{ fontStyle: "italic" }}>{truncateText(d.Name, 80)}</TableCell>
-
-                      <TableCell align="right" sx={{ whiteSpace: "nowrap" }}>
-                        <Box
-                          sx={{
-                            display: "flex",
-                            justifyContent: "flex-end",
-                            gap: 1,
-                          }}
-                        >
-                          <IconButton
-                            color="edit"
-                            onClick={() => {
-                              setDepartment(d);
-                              setMode("edit");
-                              setIsDialogFormOpen(true);
-                            }}
-                          >
-                            <EditIcon />
-                          </IconButton>
-
-                          <IconButton
-                            color="delete"
-                            onClick={() => {
-                              setDepartmentToDelete(d);
-                              setIsDialogDeleteShow(true);
-                            }}
-                          >
-                            <DeleteIcon />
-                          </IconButton>
-                        </Box>
-                      </TableCell>
-                    </TableRow>
-                  ))
-                )}
-              </TableBody>
-            </Table>
-
-            <TablePagination
-              component="div"
-              count={totalCount}
-              rowsPerPage={pageSize}
-              page={page}
-              rowsPerPageOptions={[5, 10, 25]}
-              onPageChange={(_, newPage) => {
-                setPage(newPage);
-              }}
-              onRowsPerPageChange={(event) => {
-                setPageSize(Number.parseInt(event.target.value, 10));
+        <Box>
+          <Box sx={{ display: "flex", justifyContent: "space-between", p: 2 }}>
+            <TextField
+              size="small"
+              label="Search department"
+              value={searchTerm}
+              onChange={(e) => {
                 setPage(0);
+                setSearchTerm(e.target.value);
               }}
             />
-          </TableContainer>
+
+            <Button
+              variant="contained"
+              startIcon={<AddIcon />}
+              onClick={() => {
+                setMode("new");
+                setDepartment({
+                  DepartmentID: 0,
+                  Code: "",
+                  Name: "",
+                  SaltedHashedPassword: "",
+                });
+                setIsDialogFormOpen(true);
+              }}
+            >
+              Add Department
+            </Button>
+          </Box>
+
+          <Box px={4}>
+            <TableContainer component={Paper}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>CODE</TableCell>
+                    <TableCell>NAME</TableCell>
+                    <TableCell />
+                  </TableRow>
+                </TableHead>
+
+                <TableBody>
+                  {isLoading ? (
+                    <TableRow>
+                      <TableCell colSpan={3} align="center">
+                        <CircularProgress />
+                      </TableCell>
+                    </TableRow>
+                  ) : departmentList.length === 0 ? (
+                    <TableRow>
+                      <TableCell
+                        colSpan={6}
+                        align="center"
+                        sx={{ fontWeight: "bold", py: 15 }}
+                      >
+                        No departments found.
+                      </TableCell>
+                    </TableRow>
+                  ) : (
+                    departmentList.map((d) => (
+                      <TableRow key={d.DepartmentID}>
+                        <TableCell sx={{ fontWeight: "bold" }}>
+                          {d.Code}
+                        </TableCell>
+                        <TableCell sx={{ fontStyle: "italic" }}>
+                          {truncateText(d.Name, 80)}
+                        </TableCell>
+
+                        <TableCell align="right" sx={{ whiteSpace: "nowrap" }}>
+                          <Box
+                            sx={{
+                              display: "flex",
+                              justifyContent: "flex-end",
+                              gap: 1,
+                            }}
+                          >
+                            <IconButton
+                              color="edit"
+                              onClick={() => {
+                                setDepartment(d);
+                                setMode("edit");
+                                setIsDialogFormOpen(true);
+                              }}
+                            >
+                              <EditIcon />
+                            </IconButton>
+
+                            <IconButton
+                              color="delete"
+                              onClick={() => {
+                                setDepartmentToDelete(d);
+                                setIsDialogDeleteShow(true);
+                              }}
+                            >
+                              <DeleteIcon />
+                            </IconButton>
+                          </Box>
+                        </TableCell>
+                      </TableRow>
+                    ))
+                  )}
+                </TableBody>
+              </Table>
+
+              <TablePagination
+                component="div"
+                count={totalCount}
+                rowsPerPage={pageSize}
+                page={page}
+                rowsPerPageOptions={[5, 10, 25]}
+                onPageChange={(_, newPage) => {
+                  setPage(newPage);
+                }}
+                onRowsPerPageChange={(event) => {
+                  setPageSize(Number.parseInt(event.target.value, 10));
+                  setPage(0);
+                }}
+              />
+            </TableContainer>
+          </Box>
         </Box>
-      </Box>
 
-      {/* DELETE DIALOG */}
-      <Dialog
-        open={isDialogDeleteShow}
-        onClose={() => {
-          setIsDialogDeleteShow(false);
-          setDepartmentToDelete(null);
-        }}
-      >
-        <DialogTitle sx={{backgroundColor: '#C62828',}}>Delete Department</DialogTitle>
-        <DialogContent sx={{ textAlign: "center", pt: 3 }}>
-          <img
-            src={warning}
-            alt="Warning"
-            style={{
-              width: 80,
-              height: 80,
-              marginBottom: 8,
-            }}
-          />
-          <DialogContentText>
-            {`This action cannot be undone. All data associated with ${departmentToDelete?.Code || "this department"} will be lost.`}
-          </DialogContentText>
-        </DialogContent>
-
-        <DialogActions
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            gap: 1.5,
-            pb: 3,
+        {/* DELETE DIALOG */}
+        <Dialog
+          open={isDialogDeleteShow}
+          onClose={() => {
+            setIsDialogDeleteShow(false);
+            setDepartmentToDelete(null);
           }}
         >
-          <Button
-            color="error"
-            variant="contained"
-            disabled={isOperationLoading}
-            onClick={() => handleDelete(departmentToDelete?.ID)}
-            sx={{ width: "50%" }}
-          >
-            {isOperationLoading ? <CircularProgress size={20} /> : "Confirm"}
-          </Button>
-          <Button
-            variant="outlined"
-            disabled={isOperationLoading}
-            sx={{ width: "50%" }}
-            onClick={() => {
-              setIsDialogDeleteShow(false);
-              setDepartmentToDelete(null);
+          <DialogTitle sx={{ backgroundColor: "#C62828" }}>
+            Delete Department
+          </DialogTitle>
+          <DialogContent sx={{ textAlign: "center", pt: 3 }}>
+            <img
+              src={warning}
+              alt="Warning"
+              style={{
+                width: 80,
+                height: 80,
+                marginBottom: 8,
+              }}
+            />
+            <DialogContentText>
+              {`This action cannot be undone. All data associated with ${departmentToDelete?.Code || "this department"} will be lost.`}
+            </DialogContentText>
+          </DialogContent>
+
+          <DialogActions
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              gap: 1.5,
+              pb: 3,
             }}
           >
-            Cancel
-          </Button>
-        </DialogActions>
-      </Dialog>
+            <Button
+              color="error"
+              variant="contained"
+              disabled={isOperationLoading}
+              onClick={() => handleDelete(departmentToDelete?.DepartmentID)}
+              sx={{ width: "50%" }}
+            >
+              {isOperationLoading ? <CircularProgress size={20} /> : "Confirm"}
+            </Button>
+            <Button
+              variant="outlined"
+              disabled={isOperationLoading}
+              sx={{ width: "50%" }}
+              onClick={() => {
+                setIsDialogDeleteShow(false);
+                setDepartmentToDelete(null);
+              }}
+            >
+              Cancel
+            </Button>
+          </DialogActions>
+        </Dialog>
 
-      {/* FORM DIALOG */}
-      <Dialog open={isDialogFormOpen} onClose={() => setIsDialogFormOpen(false)}>
-        <DialogTitle sx={{ backgroundColor: '#2e6417' }}>{mode === "new" ? "Add Department" : "Edit Department"}</DialogTitle>
+        {/* FORM DIALOG */}
+        <Dialog
+          open={isDialogFormOpen}
+          onClose={() => setIsDialogFormOpen(false)}
+        >
+          <DialogTitle sx={{ backgroundColor: "#2e6417" }}>
+            {mode === "new" ? "Add Department" : "Edit Department"}
+          </DialogTitle>
 
-        <DialogContent>
-          <TextField
-            margin="dense"
-            label="Department Code"
-            fullWidth
-            value={department.Code}
-            onChange={(e) =>
-              setDepartment((p) => ({ ...p, Code: e.target.value }))
-            }
-          />
+          <DialogContent>
+            <TextField
+              margin="dense"
+              label="Department Code"
+              fullWidth
+              value={department.Code}
+              onChange={(e) =>
+                setDepartment((p) => ({ ...p, Code: e.target.value }))
+              }
+            />
 
-          <TextField
-            margin="dense"
-            label="Department Name"
-            fullWidth
-            value={department.Name}
-            onChange={(e) =>
-              setDepartment((p) => ({ ...p, Name: e.target.value }))
-            }
-          />
-        </DialogContent>
+            <TextField
+              margin="dense"
+              label="Department Name"
+              fullWidth
+              value={department.Name}
+              onChange={(e) =>
+                setDepartment((p) => ({ ...p, Name: e.target.value }))
+              }
+            />
 
-        <DialogActions>
-          <Button onClick={handleSave} variant="contained">
-            Save
-          </Button>
-          <Button onClick={() => setIsDialogFormOpen(false)}>Cancel</Button>
-        </DialogActions>
-      </Dialog>
+            <TextField
+              margin="dense"
+              label="Department Password"
+              type="password"
+              fullWidth
+              value={department.SaltedHashedPassword || ""}
+              onChange={(e) =>
+                setDepartment((p) => ({
+                  ...p,
+                  SaltedHashedPassword: e.target.value,
+                }))
+              }
+              helperText="Password must be at least 8 characters long"
+            />
+          </DialogContent>
+
+          <DialogActions>
+            <Button onClick={handleSave} variant="contained">
+              Save
+            </Button>
+            <Button onClick={() => setIsDialogFormOpen(false)}>Cancel</Button>
+          </DialogActions>
+        </Dialog>
       </MainHeader>
     </>
   );
